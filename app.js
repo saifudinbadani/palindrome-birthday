6f8f42ce5055205a69f6813cb6f7e8f96f061d56
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2021-08-16 17:47:04 +0800

    nextDate and leapyear function built

--- app.js
+++ app.js
@@ -20,11 +20,7 @@ function isPalindrome(str){
     }
 }
 
-var date ={
-    day: 2,
-    month: 5,
-    year: 2020
-}
+
 function dateToStr(date){
     var dateStr = {day: '', month: '', year: ''}
     if(date.day < 10){
@@ -55,4 +51,80 @@ function getAllDateFormats(date){
     return [ddmmyyy, mmddyyy, yyyymmdd, mmddyy, yymmdd]
 }
 
-function
+function isPalindromeForAllFormats(date){
+    // putting all formatted dates in list
+    var listofAllFormattedDates = getAllDateFormats(date);
+    var ifPalindrome = false;
+    for(var i=0; i<listofAllFormattedDates.length; i++){
+        if (isPalindrome(listofAllFormattedDates[i])){
+            ifPalindrome = true;
+            break;
+        }
+
+    }
+    return ifPalindrome;
+}
+
+// helper leapyear function
+function isLeapYear(year){
+    if (year % 400 === 0)
+    return true;
+
+  if (year % 100 === 0)
+    return false;
+
+  if (year % 4 === 0)
+    return true;
+
+  return false;
+}
+
+//function to increment date 
+function getNextDate(date){
+    var day = date.day + 1;
+    var month = date.month;
+    var year = date.year;
+
+    var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30]
+
+    if(month === 2){
+        if (isLeapYear(year)){
+            if(day>29){
+                day=1;
+                month++;
+            }
+            
+        } else{
+            if (day > 28){
+                day = 1;
+                month++;
+            }
+        }
+    }
+    else{
+        if ( day > daysInMonth[month -1]){
+            day = 1;
+            month++;
+        }
+    } 
+    if (month > 12){
+        month=1;
+        year++;
+    }
+
+    return {
+        day: day,
+        month: month,
+        year: year
+    }
+}
+
+
+
+var date ={
+    day: 28,
+    month: 2,
+    year: 2020
+}
+
+console.log(getNextDate(date))
