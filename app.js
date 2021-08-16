26bb919675800d34676261c3467a8f4e692b2a98
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2021-08-16 16:54:47 +0800

    reverse string logic

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ var checkButton = document.querySelector('#checkButton');
 var outputResult = document.querySelector('#outputResult');
 
 
+function reverseString(str){
+    var listChars = str.split('');
+    var reverseChars = listChars.reverse();
+    var reverseJoinedStr = reverseChars.join('');
+
+    console.log(reverseJoinedStr)
+}
+
+
+reverseString('Hello')
