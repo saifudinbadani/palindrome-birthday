d1a662104a3fd4f536005bcbc71ca84733a6aa78
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2021-08-16 17:12:36 +0800

    till getAllDatFormats logic build

--- app.js
+++ app.js
@@ -8,8 +8,51 @@ function reverseString(str){
     var reverseChars = listChars.reverse();
     var reverseJoinedStr = reverseChars.join('');
 
-    console.log(reverseJoinedStr)
+    return reverseJoinedStr;
 }
 
 
-reverseString('Hello')
+function isPalindrome(str){
+    if(str === reverseString(str)){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+var date ={
+    day: 2,
+    month: 5,
+    year: 2020
+}
+function dateToStr(date){
+    var dateStr = {day: '', month: '', year: ''}
+    if(date.day < 10){
+        // concatenating number with string gives string
+        dateStr.day = '0' + date.day;
+    } else{
+        dateStr.day = date.day.toString();
+    }
+    if(date.month < 10){
+        // concatenating number with string gives string
+        dateStr.month = '0' + date.month;
+    } else{
+        dateStr.month = date.month.toString();
+    }
+    dateStr.year = date.year.toString();
+
+    return dateStr
+}
+
+function getAllDateFormats(date){
+    var dateStr = dateToStr(date);
+    var ddmmyyy = dateStr.day + dateStr.month + dateStr.year;
+    var mmddyyy = dateStr.month + dateStr.day + dateStr.year;
+    var yyyymmdd = dateStr.year + dateStr.month + dateStr.day;
+    var mmddyy = dateStr.month + dateStr.day + dateStr.year.slice(-2);
+    var yymmdd = dateStr.year.slice(-2) + dateStr.month + dateStr.day;
+
+    return [ddmmyyy, mmddyyy, yyyymmdd, mmddyy, yymmdd]
+}
+
+function
